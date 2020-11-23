23a071ba58461aa5bbffaf31ed7cdc12ce8eaefe
Sinem Ipek <Sinem.Ipek@logo.com.tr>
2020-11-23 16:46:59 +0300

    Add project files.

---
+++ Orders/Services/CustomerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orders.Models;
+
+namespace Orders.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private IList<Customer> _customers;
+
+        public CustomerService()
+        {
+            _customers = new List<Customer>();
+            _customers.Add(new Customer(1, "Walter White"));
+            _customers.Add(new Customer(2, "Phil Dunphy"));
+            _customers.Add(new Customer(3, "Beth Harmon"));
+            _customers.Add(new Customer(4, "Rachel Green"));
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return GetCustomerByIdAsync(id).Result;
+        }
+
+        public Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            return Task.FromResult(_customers.Single(o => Equals(o.Id, id)));
+        }
+
+        public Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            return Task.FromResult(_customers.AsEnumerable());
+        }
+    }
+
+    public interface ICustomerService
+    {
+        Customer GetCustomerById(int id);
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+    }
+}
